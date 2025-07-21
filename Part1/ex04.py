341422563988146b068a5e6bc8864f9e7eeb0b51
nerineeeeee <nerinechankeshuen109@gmail.com>
2025-07-21 05:34:08 +0000

    done

---
+++ Part1/ex04.py
@@ -0,0 +1,17 @@
+# num1 and num2 are integers
+num1 = 100
+num2 = 2
+
+# num3 and num4 are strings
+num3 = "50"
+num4 = "5"
+
+# ✅ Convert string variables to integers
+num3 = int(num3)
+num4 = int(num4)
+
+# This will work fine
+print(num1 / num2)
+
+# This will now also work fine
+print(num3 / num4)
---
+++ Part1/ex05.py
@@ -0,0 +1,11 @@
+# Word variables
+word1 = 'car '
+word2 = 'exceeded '
+text1 = 'speed limit '
+word3 = 'the '
+
+# Combine the words into a sentence
+sentence = word3 + word1 + word2 + word3 + text1
+
+# Display the sentence
+print(sentence)
